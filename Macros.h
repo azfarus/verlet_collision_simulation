3e7ee9834fae3cded6c26ca30850c84aba31b829
azfarus <samazfar52@gmail.com>
2024-08-10 01:30:29 +0600

    rect

---
+++ Macros.h
@@ -0,0 +1,36 @@
+#pragma once
+#include <random>
+#include  <chrono>
+#include <SFML/Graphics.hpp>
+#include <algorithm>
+#include <cmath>
+#include<sstream>
+#include "Helper.h"
+
+double getRandomdouble(double min, double max);
+
+
+#define SCREEN_WIDTH 1200
+#define SCREEN_HEIGHT 1200
+#define BOX_WIDTH 280.0f
+#define BOX_HEIGHT 200.0f
+#define g_VALUE sf::Vector2f(0,-200)
+#define BALL_COUNT 1024
+#define BALL_RADIUS 10
+#define FRAME_COUNT 2000
+#define FPS 30.0
+#define FRAME_TIME (1.0/FPS)
+#define MAX_VEL_COMPONENT 1000.0f
+#define SUBFRAMES 30.0
+#define EFFICIENCY 0.80f
+#define SPACING .5
+#define OFFSET 100.0
+#define COLLISION_OFFSET .8f
+#define SINGLE_SPAWN_FRAME_INTERVAL 1
+
+
+
+
+
+#define RANDOM_VEC2F(min,max) \
+    sf::Vector2f( getRandomdouble(min,max) , getRandomdouble(min,max) )
---
+++ Source.cpp
@@ -0,0 +1,43 @@
+
+#include "Handler.h"
+#include <sstream>
+#include<iostream>
+int main()
+{
+    sf::RenderTexture renderTexture;
+    if (!renderTexture.create(SCREEN_WIDTH, SCREEN_HEIGHT)) {
+        return EXIT_FAILURE;
+    }
+
+
+    Handler handler = Handler(BALL_COUNT, BALL_RADIUS);
+    
+    
+
+
+    int t = FRAME_COUNT;
+    std::cout << "Start\n";
+
+    while(t--) {
+        
+        
+        renderTexture.clear(sf::Color::Black);
+
+
+        int buff = SUBFRAMES;
+        while(buff--) handler.update((FRAME_TIME)/SUBFRAMES);
+        if (t % SINGLE_SPAWN_FRAME_INTERVAL == 0 ) {
+            handler.singleSpawner(sf::Vector2f(SCREEN_WIDTH / 2, SCREEN_HEIGHT - 100.0f) ,g_VALUE , sf::Vector2f(-40,0));
+                
+        }
+
+        handler.draw(renderTexture);
+        
+        sf::Image screenshot = renderTexture.getTexture().copyToImage();
+        std::string filename = "video\\frame" + std::to_string(FRAME_COUNT-t) + ".png"; // Change the filename format as needed
+        screenshot.saveToFile(filename);
+        std::cout << "Frame: "<<FRAME_COUNT-t<<"\r";
+    }
+    std::cout << "Fin";
+    return 0;
+}
